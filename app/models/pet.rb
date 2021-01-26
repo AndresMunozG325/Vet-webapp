91b3aab0eb27f00a33aeb47b408b241e92ba53a3
Andres <andresmunozgo@gmail.com>
2021-01-26 14:41:25 -0300

    Relationship between models is created

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,7 @@
 class Pet < ApplicationRecord
+    has_many :pet_histories, dependent: :destroy
+    validates :name, presence: true
+    validates :breed, presence: true
+    validates :birthdate, presence: true
+     
 end
